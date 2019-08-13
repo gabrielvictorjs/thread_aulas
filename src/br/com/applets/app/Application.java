f9cdad2fa66aa07b1152bd8191ee4d99c1446523
Gabriel Victor <dester.gvpdv@gmail.com>
2019-08-13 15:44:34 -0300

    primeira aula de thread

--- src/br/com/applets/app/Application.java
+++ src/br/com/applets/app/Application.java
@@ -1,16 +1,45 @@
 package br.com.applets.app;
 
-import java.awt.Graphics;
-
 import javax.swing.JApplet;
 
-public class Application extends JApplet {
+public class Application extends JApplet implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 	
+	Thread thread; boolean exe = false; int x = 0;
 	
-	public void paint(Graphics graph) {
-		graph.drawLine(20, 20, 200, 200);
+	public void init() {
+		thread = new Thread(this);
+		showNotify();
+	}
+	
+	public void showNotify() {
+		thread.start();
+		exe = true;
+	}
+	
+	public void hideNotify() {
+		thread = null;
+		exe = false;
+	}
+
+
+	@Override
+	public void run() {
+		while(exe && x < 50) {
+			System.out.println(x++ +"\n");
+			try {
+				Thread.sleep(100);
+			} catch(Exception e) {};
+		}
+		
+		while(exe) {
+			System.out.println(x-- +"\n");
+			if(x < 0) System.exit(1);
+			try {
+				Thread.sleep(100);
+			} catch(Exception e) {};
+		}
 	}
 
 }
